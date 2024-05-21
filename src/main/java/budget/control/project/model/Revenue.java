bbe81a4308693bce5b35406638d1ec6324bd0779
Davi Santos <davi.santos@liferay.com>
2024-05-21 15:08:33 -0300

    feature: add revenue entity

---
+++ src/main/java/budget/control/project/model/Revenue.java
@@ -0,0 +1,49 @@
+package budget.control.project.model;
+
+import budget.control.project.dto.RevenueDTORequest;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity(name = "Revenue")
+@Table(name = "revenues")
+public class Revenue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private double amount;
+    private LocalDate date;
+
+    public Revenue(RevenueDTORequest revenueDTORequest) {
+        this.description = revenueDTORequest.getDescription();
+        this.amount = revenueDTORequest.getAmount();
+        this.date = revenueDTORequest.getDate();
+    }
+
+    public Revenue(String description, double amount, LocalDate date) {
+        this.description = description;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+}
