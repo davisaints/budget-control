db1843bbd0f814f46bd5a0105c9d0c47fdbdecf0
Davi Santos <davi.santos@liferay.com>
2024-06-11 22:12:21 -0300

    feat(controller): add GET endpoint for retrieving all revenues

--- src/main/java/budget/control/project/controller/RevenueController.java
+++ src/main/java/budget/control/project/controller/RevenueController.java
@@ -5,9 +5,13 @@ import budget.control.project.dto.RevenueDTOResponse;
 import budget.control.project.service.RevenueService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -19,5 +23,10 @@ public class RevenueController {
     @PostMapping
     public ResponseEntity<RevenueDTOResponse> postRevenue(@RequestBody @Valid RevenueDTORequest revenueDTORequest) {
         return new ResponseEntity<>(revenueService.post(revenueDTORequest), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RevenueDTOResponse>> getAll(@PageableDefault(size = 10, sort = {"date"}) Pageable pageable) {
+        return new ResponseEntity<>(revenueService.getAll(pageable), HttpStatus.OK);
     }
 }
