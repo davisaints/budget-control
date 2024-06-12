12383fa2e2ecc5aef8b53f225f4babb9b3c697aa
Davi Santos <davi.santos@liferay.com>
2024-06-11 22:09:58 -0300

    feat(service): add getAll method to RevenueService

--- src/main/java/budget/control/project/service/RevenueService.java
+++ src/main/java/budget/control/project/service/RevenueService.java
@@ -2,7 +2,12 @@ package budget.control.project.service;
 
 import budget.control.project.dto.RevenueDTORequest;
 import budget.control.project.dto.RevenueDTOResponse;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface RevenueService {
     RevenueDTOResponse post(RevenueDTORequest revenueDTORequest);
+    
+    List<RevenueDTOResponse> getAll(Pageable pageable);
 }
