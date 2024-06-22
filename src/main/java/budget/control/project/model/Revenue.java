2f48a4a44dbc14ad57a3764b0ad10adb14564897
Davi Santos <davi.santos@liferay.com>
2024-06-22 18:39:40 -0300

    feat(entity): add setter methods for PUT endpoint

--- src/main/java/budget/control/project/model/Revenue.java
+++ src/main/java/budget/control/project/model/Revenue.java
@@ -39,11 +39,23 @@ public class Revenue {
         return description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public double getAmount() {
         return amount;
     }
 
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
     public LocalDate getDate() {
         return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
     }
 }
