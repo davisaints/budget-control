4029ccc53d9e25c305b89db591b600057a93a6f9
Davi Santos <davi.santos@liferay.com>
2024-07-01 20:42:26 -0300

    feat(controller): add GET endpoint

--- src/main/java/budget/control/project/controller/ExpenseController.java
+++ src/main/java/budget/control/project/controller/ExpenseController.java
@@ -5,9 +5,13 @@ import budget.control.project.dto.ExpenseDTOResponse;
 import budget.control.project.service.ExpenseService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -17,9 +21,14 @@ public class ExpenseController {
     @Autowired
     ExpenseService expenseService;
 
+    @GetMapping
+    public ResponseEntity<List<ExpenseDTOResponse>> getAll(@PageableDefault(size = 10, sort = {"date"}) Pageable pageable) {
+        return new ResponseEntity<>(expenseService.getAll(pageable), HttpStatus.OK);
+    }
+
     @PostMapping
     public ResponseEntity<ExpenseDTOResponse> post(@RequestBody @Valid ExpenseDTORequest expenseDTORequest) {
         return new ResponseEntity<>(expenseService.post(expenseDTORequest), HttpStatus.CREATED);
     }
-    
+
 }
