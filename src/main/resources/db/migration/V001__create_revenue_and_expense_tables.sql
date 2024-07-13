33038cc1c6a2c631ec2fa0a673db7a2327e6102c
Davi Santos <davi.santos@liferay.com>
2024-07-13 14:19:40 -0300

    refactor(database): rename 'date' column to 'transaction_date'

--- src/main/resources/db/migration/V001__create_revenue_and_expense_tables.sql
+++ src/main/resources/db/migration/V001__create_revenue_and_expense_tables.sql
@@ -2,12 +2,12 @@ CREATE TABLE IF NOT EXISTS revenue (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     description VARCHAR(70) NOT NULL,
     amount DECIMAL(19, 4) NOT NULL,
-    date TIMESTAMP
+    transaction_date TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS expense (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     description VARCHAR(70) NOT NULL,
     amount DECIMAL(19, 4) NOT NULL,
-    date TIMESTAMP
+    transaction_date TIMESTAMP
 );
