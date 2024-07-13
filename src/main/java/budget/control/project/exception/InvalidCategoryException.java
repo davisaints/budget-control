7e31c608cfdf65ceab989c5298b1fa8dfb434698
Davi Santos <davi.santos@liferay.com>
2024-07-12 23:59:11 -0300

    feat(service): add custom exception for invalid category

---
+++ src/main/java/budget/control/project/exception/InvalidCategoryException.java
@@ -0,0 +1,8 @@
+package budget.control.project.exception;
+
+public class InvalidCategoryException extends RuntimeException {
+
+  public InvalidCategoryException(String message) {
+    super();
+  }
+}
