b6a2ec4108808784dccb98f8cf2e78049283c656
Davi Santos <davi.santos@liferay.com>
2024-07-19 14:21:49 -0300

    feat(repository): add method to get revenues/expenses by year and month

--- src/main/java/budget/control/project/repository/ExpenseRepository.java
+++ src/main/java/budget/control/project/repository/ExpenseRepository.java
@@ -5,10 +5,17 @@ import java.time.LocalDate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+
+  @Query(
+      "select e from Expense e where year(e.transactionDate) = :year and month(e.transactionDate) = :month")
+  Page<Expense> findByYearAndMonth(
+      @Param("year") Integer year, @Param("month") Integer month, Pageable pageable);
 
   Expense findByDescriptionAndTransactionDate(String description, LocalDate transactionDate);
 
--- src/main/java/budget/control/project/repository/RevenueRepository.java
+++ src/main/java/budget/control/project/repository/RevenueRepository.java
@@ -2,13 +2,22 @@ package budget.control.project.repository;
 
 import budget.control.project.model.Revenue;
 import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RevenueRepository extends JpaRepository<Revenue, Long> {
+
+  @Query(
+      "select r from Revenue r where year(r.transactionDate) = :year and month(r.transactionDate) = :month")
+  Page<Revenue> findByYearAndMonth(
+      @Param("year") Integer year, @Param("month") Integer month, Pageable pageable);
 
   Revenue findByDescriptionAndTransactionDate(String description, LocalDate transactionDate);
 
