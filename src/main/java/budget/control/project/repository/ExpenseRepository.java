40fd858fc6bcc1a3262dc1eefa89b7108c77664c
Davi Santos <davi.santos@liferay.com>
2024-07-26 12:58:02 -0300

    feat(repository): add method to find the total of expense per category

--- src/main/java/budget/control/project/repository/ExpenseRepository.java
+++ src/main/java/budget/control/project/repository/ExpenseRepository.java
@@ -1,8 +1,10 @@
 package budget.control.project.repository;
 
+import budget.control.project.dto.CategoryExpenseDTOResponse;
 import budget.control.project.model.Expense;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -28,4 +30,13 @@ public interface ExpenseRepository extends JpaRepository<Expense, Long> {
           + " FROM Expense e"
           + " WHERE YEAR(e.transactionDate) = :year AND MONTH(e.transactionDate) = :month")
   BigDecimal findTotalMonthlyExpense(@Param("year") Integer year, @Param("month") Integer month);
+
+  @Query(
+      "SELECT new budget.control.project.dto.CategoryExpenseDTOResponse(c.name, SUM(e.amount)) "
+          + "FROM Expense e JOIN e.category c "
+          + "WHERE YEAR(e.transactionDate) = :year AND MONTH(e.transactionDate) = :month "
+          + "GROUP BY c.name "
+          + "ORDER BY SUM(e.amount) DESC")
+  List<CategoryExpenseDTOResponse> findTotalExpensesPerCategory(
+      @Param("year") Integer year, @Param("month") Integer month);
 }
