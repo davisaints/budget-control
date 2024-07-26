aba30e6313af21d834b262185c2e652ed895ad20
Davi Santos <davi.santos@liferay.com>
2024-07-26 10:38:14 -0300

    feat(exception handling): handle MethodArgumentNotValidException

--- src/main/java/budget/control/project/exception/GlobalExceptionHandler.java
+++ src/main/java/budget/control/project/exception/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package budget.control.project.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -45,5 +49,24 @@ public class GlobalExceptionHandler {
     logger.severe(ex.getMessage());
 
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ERROR: " + ex.getMessage());
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Map<String, String>> handleValidationExceptions(
+      MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+
+    ex.getBindingResult()
+        .getAllErrors()
+        .forEach(
+            (error) -> {
+              String fieldName = ((FieldError) error).getField();
+              String errorMessage = error.getDefaultMessage();
+              errors.put(fieldName, errorMessage);
+            });
+
+    logger.severe(errors.values().iterator().next());
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
   }
 }
