dabd8d8e06e7b9e3a62a96368c10798b20f59d0a
Davi Santos <davi.santos@liferay.com>
2024-07-29 12:50:16 -0300

    chore: add database migrations for tests

---
+++ src/test/resources/db-test/V002__create_category_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS category (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(70) NOT NULL UNIQUE
+);
+
+MERGE INTO category (name)
+KEY(name)
+VALUES ('Education'),
+       ('Food'),
+       ('Health'),
+       ('Rent'),
+       ('Other'),
+       ('Recreation'),
+       ('Transportation'),
+       ('Unexpected expense');
