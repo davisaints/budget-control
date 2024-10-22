d499799de482b8bc05a40ba533b6aa4b58bd9dfd
Davi Santos <davi.santos4357@gmail.com>
2024-10-22 20:55:38 -0300

    feat: create user DTOs for authentication

---
+++ src/main/java/budget/control/project/dto/request/LoginUserDTO.java
@@ -0,0 +1,28 @@
+package budget.control.project.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class LoginUserDTO {
+
+  @NotBlank(message = "Username is mandatory")
+  private String username;
+
+  @NotBlank(message = "Password is mandatory")
+  private String password;
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+}
