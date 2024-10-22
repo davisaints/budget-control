fe2ac56cf72d142ffda092313a0ef17e89299555
Davi Santos <davi.santos4357@gmail.com>
2024-10-22 20:58:41 -0300

    feat: add security-related exception handling

--- src/main/java/budget/control/project/exception/GlobalExceptionHandler.java
+++ src/main/java/budget/control/project/exception/GlobalExceptionHandler.java
@@ -1,72 +1,115 @@
 package budget.control.project.exception;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.persistence.EntityNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Logger;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AccountStatusException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-  private static final Logger logger = Logger.getLogger(GlobalExceptionHandler.class.getName());
+    private static final Logger logger = Logger.getLogger(GlobalExceptionHandler.class.getName());
 
-  @ExceptionHandler(DuplicateExpenseException.class)
-  public ResponseEntity<String> handleDuplicateExpenseException(DuplicateExpenseException ex) {
-    logger.severe(ex.getMessage());
+    @ExceptionHandler(DuplicateExpenseException.class)
+    public ResponseEntity<String> handleDuplicateExpenseException(DuplicateExpenseException ex) {
+        logger.severe(ex.getMessage());
 
-    return ResponseEntity.status(HttpStatus.CONFLICT).body("ERROR: " + ex.getMessage());
-  }
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("ERROR: " + ex.getMessage());
+    }
 
-  @ExceptionHandler(DuplicateRevenueException.class)
-  public ResponseEntity<String> handleDuplicateRevenueException(DuplicateRevenueException ex) {
-    logger.severe(ex.getMessage());
+    @ExceptionHandler(DuplicateRevenueException.class)
+    public ResponseEntity<String> handleDuplicateRevenueException(DuplicateRevenueException ex) {
+        logger.severe(ex.getMessage());
 
-    return ResponseEntity.status(HttpStatus.CONFLICT).body("ERROR: " + ex.getMessage());
-  }
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("ERROR: " + ex.getMessage());
+    }
 
-  @ExceptionHandler(EntityNotFoundException.class)
-  public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
-    logger.severe(ex.getMessage());
+    @ExceptionHandler(DuplicateUserException.class)
+    public ResponseEntity<String> handleDuplicateUserException(DuplicateUserException ex) {
+        logger.severe(ex.getMessage());
 
-    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ERROR: " + ex.getMessage());
-  }
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("ERROR: " + ex.getMessage());
+    }
 
-  @ExceptionHandler(IllegalArgumentException.class)
-  public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
-    logger.severe(ex.getMessage());
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
+        logger.severe(ex.getMessage());
 
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ERROR: " + ex.getMessage());
-  }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ERROR: " + ex.getMessage());
+    }
 
-  @ExceptionHandler(InvalidCategoryException.class)
-  public ResponseEntity<String> handleInvalidCategoryException(InvalidCategoryException ex) {
-    logger.severe(ex.getMessage());
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        logger.severe(ex.getMessage());
 
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ERROR: " + ex.getMessage());
-  }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ERROR: " + ex.getMessage());
+    }
 
-  @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<Map<String, String>> handleValidationExceptions(
-      MethodArgumentNotValidException ex) {
-    Map<String, String> errors = new HashMap<>();
+    @ExceptionHandler(InvalidCategoryException.class)
+    public ResponseEntity<String> handleInvalidCategoryException(InvalidCategoryException ex) {
+        logger.severe(ex.getMessage());
 
-    ex.getBindingResult()
-        .getAllErrors()
-        .forEach(
-            (error) -> {
-              String fieldName = ((FieldError) error).getField();
-              String errorMessage = error.getDefaultMessage();
-              errors.put(fieldName, errorMessage);
-            });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ERROR: " + ex.getMessage());
+    }
 
-    logger.severe(errors.values().iterator().next());
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
 
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
-  }
+        ex.getBindingResult()
+                .getAllErrors()
+                .forEach(
+                        (error) -> {
+                            String fieldName = ((FieldError) error).getField();
+                            String errorMessage = error.getDefaultMessage();
+                            errors.put(fieldName, errorMessage);
+                        });
+
+        logger.severe(errors.values().iterator().next());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handleSecurityException(Exception exception) {
+        exception.printStackTrace();
+        ProblemDetail errorDetail = null;
+
+        if (exception instanceof BadCredentialsException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), exception.getMessage());
+            errorDetail.setProperty("description", "The username or password is incorrect");
+
+            return errorDetail;
+        }
+
+        if (exception instanceof AccountStatusException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
+            errorDetail.setProperty("description", "The account is locked");
+        }
+
+        if (exception instanceof ExpiredJwtException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
+            errorDetail.setProperty("description", "The JWT token has expired");
+        }
+
+        if (errorDetail == null) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(500), exception.getMessage());
+            errorDetail.setProperty("description", "Unknown internal server error.");
+        }
+
+        return errorDetail;
+    }
 }
