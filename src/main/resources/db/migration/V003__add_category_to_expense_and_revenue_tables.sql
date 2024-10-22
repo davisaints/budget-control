f67f1cf729934eb2558a3a43183d8b7617b233f6
Davi Santos <davi.santos4357@gmail.com>
2024-10-22 20:59:48 -0300

    fix: prevent duplicate column and constraint errors in expense table migration

--- src/main/resources/db/migration/V003__add_category_to_expense_and_revenue_tables.sql
+++ src/main/resources/db/migration/V003__add_category_to_expense_and_revenue_tables.sql
@@ -1,4 +1,14 @@
-ALTER TABLE expense
-ADD COLUMN category_id INT NOT NULL;
+DO $$
+    BEGIN
+        IF NOT EXISTS (
+            SELECT 1
+            FROM information_schema.columns
+            WHERE table_name = 'expense' AND column_name = 'category_id'
+        ) THEN
+            ALTER TABLE expense
+                ADD COLUMN category_id INT NOT NULL;
 
-ALTER TABLE expense ADD CONSTRAINT fk_expense_category FOREIGN KEY (category_id) REFERENCES category (id);
+            ALTER TABLE expense
+                ADD CONSTRAINT fk_expense_category FOREIGN KEY (category_id) REFERENCES category (id);
+        END IF;
+    END $$;
