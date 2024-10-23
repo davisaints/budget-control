43de671703675d7a029bcaa459c1878877a2a7f2
Davi Santos <davi.santos4357@gmail.com>
2024-10-23 19:09:35 -0300

    refactor: use CrudRepository instead

--- src/main/java/budget/control/project/repository/UserRepository.java
+++ src/main/java/budget/control/project/repository/UserRepository.java
@@ -1,13 +1,12 @@
 package budget.control.project.repository;
 
 import budget.control.project.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends CrudRepository<User, String> {
 
-    Optional<User> findByUsername(String username);
+  Optional<User> findByUsername(String username);
 }
