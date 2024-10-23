a3e9f21e365ac9e91be7e8c8f8da4777a5237d4f
Davi Santos <davi.santos4357@gmail.com>
2024-10-22 21:23:00 -0300

    style: source format

--- src/main/java/budget/control/project/repository/UserRepository.java
+++ src/main/java/budget/control/project/repository/UserRepository.java
@@ -2,7 +2,6 @@ package budget.control.project.repository;
 
 import budget.control.project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
-  Optional<User> findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
